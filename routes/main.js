b5c0f2c2b940dbb3042f566333110711e2332bf3
Shahboz <83070485+Shahboz007@users.noreply.github.com>
2024-02-19 08:27:54 +0500

    done main page

--- routes/main.js
+++ routes/main.js
@@ -3,11 +3,13 @@ const {Router} = require('express');
 const router = Router();
 const path = require('path')
 
-const users = require("./users")
+const {users} = require("./users")
 
-router.get('/', (req, res) => {
-  console.log(users.users)
-  res.render('main')
+router.get('/', (req, res) => { 
+  res.render('main',{
+    title: 'Users list',
+    users,
+  })
   // res.sendFile('../views')
   // res.sendFile(path.join(__dirname, '..','views','main.html'));
 })
