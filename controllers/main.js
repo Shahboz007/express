7c7b58424e861d0699b8fc564d314625eac38f90
shahboz007 <olmasovshahboz007@gmail.com>
2024-02-23 20:44:55 +0500

    added getAllUser from database

--- controllers/main.js
+++ controllers/main.js
@@ -1,15 +1,22 @@
 const User = require("../models/users");
+const pool = require("../config/db");
 
 // Route     /
 // Method    GET
 // Descr     GET main page
-const getMainPage = (req, res) => {
-    const users = User.findAll();
+const getMainPage = async (req, res) => {
+    try {
+        const users = await pool.query('SELECT * FROM user_info');
+        
 
-    res.render("main", {
-        title: "Users list",
-        users,
-    });
+        console.log(users.rows)
+        res.render("main", {
+            title: "Users list",
+            users:users.rows,
+        });
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 // Route     /:uid
