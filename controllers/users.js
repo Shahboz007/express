fc0f4a372fed1d05f90b74de8bc4b4d1078f8f6f
shahboz007 <olmasovshahboz007@gmail.com>
2024-02-23 21:06:13 +0500

    added addNewUser

--- controllers/users.js
+++ controllers/users.js
@@ -1,29 +1,36 @@
 const uuid = require("uuid");
+const User = require("../models/users");
+const pool = require("../config/db");
 
 // Route     /add-users
 // Method    GET
-
-const User = require("../models/users");
-
 // Descr     GET add-users page
 const getAddUsersPage = (req, res) => {
-    res.render("add-users", {
-        title: "Add new user",
-    });
+  res.render("add-users", {
+    title: "Add new user",
+  });
 };
 
 // Route     /users
 // Method    POST
 // Descr     add new user
-const addNewUser = (req, res) => {
-    const uid = uuid.v4();
-
-    const users = new User(uid, req.body.username, req.body.age);
-    users.save();
+const addNewUser = async (req, res) => {
+  try {
+    const newUser = await pool.query(
+      `
+            INSERT INTO user_info (username, age) 
+            VALUES ($1, $2) RETURNING *
+        `,
+      [req.body.username, req.body.age]
+    );
+    console.log(newUser);
     res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = {
-    getAddUsersPage,
-    addNewUser,
+  getAddUsersPage,
+  addNewUser,
 };
