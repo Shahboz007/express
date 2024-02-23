134c858488371b875db005806718c38ec7c15d1d
shahboz007 <olmasovshahboz007@gmail.com>
2024-02-23 21:19:31 +0500

    added edit page and requests

--- controllers/users.js
+++ controllers/users.js
@@ -16,14 +16,46 @@ const getAddUsersPage = (req, res) => {
 // Descr     add new user
 const addNewUser = async (req, res) => {
   try {
-    const newUser = await pool.query(
+    await pool.query(
       `
             INSERT INTO user_info (username, age) 
             VALUES ($1, $2) RETURNING *
         `,
       [req.body.username, req.body.age]
     );
-    console.log(newUser);
+
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Route     /users/edit/:id
+// Method    GET
+// Descr     edit user
+const updatePageUser = async (req, res) => {
+  try {
+    const user = await pool.query("SELECT * FROM user_info WHERE id = $1", [
+      req.params.id,
+    ]);
+
+    res.render("edit-user", {
+      title: "Edit page",
+      user: user.rows[0],
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Route     /users/edit/:id
+// Method    POST
+// Descr     edit user
+const updateUser = async (req, res) => {
+  try {
+    await pool.query('UPDATE user_info SET username = $1, age = $2 WHERE id = $3',
+    [req.body.username, req.body.age, req.params.id])
+
     res.redirect("/");
   } catch (error) {
     console.log(error);
@@ -33,4 +65,6 @@ const addNewUser = async (req, res) => {
 module.exports = {
   getAddUsersPage,
   addNewUser,
+  updatePageUser,
+  updateUser,
 };
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const {getAddUsersPage, addNewUser} = require('../controllers/users')
- 
+const {
+  getAddUsersPage,
+  addNewUser,
+  updatePageUser,
+  updateUser,
+} = require("../controllers/users");
 
 router.get("/add-users", getAddUsersPage);
-
+router.get("/edit/:id", updatePageUser);
+router.post("/edit/:id", updateUser);
 router.post("/users", addNewUser);
 
 exports.router = router;
