9a456d295b30eeaf374fe4678302096a0faa2619
shahboz007 <olmasovshahboz007@gmail.com>
2024-02-23 20:54:39 +0500

    added get by id

--- controllers/main.js
+++ controllers/main.js
@@ -5,33 +5,38 @@ const pool = require("../config/db");
 // Method    GET
 // Descr     GET main page
 const getMainPage = async (req, res) => {
-    try {
-        const users = await pool.query('SELECT * FROM user_info');
-        
+  try {
+    const users = await pool.query("SELECT * FROM user_info");
 
-        console.log(users.rows)
-        res.render("main", {
-            title: "Users list",
-            users:users.rows,
-        });
-    } catch (error) {
-        console.log(error);
-    }
+    console.log(users.rows);
+    res.render("main", {
+      title: "Users list",
+      users: users.rows,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 // Route     /:uid
 // Method    GET
 // Descr     Get user page by uid
-const getUserPageByUid = (req, res) => {
-    const user = User.findByUid(req.params.uid);
+const getUserPageByUid = async (req, res) => {
+  try {
+    const user = await pool.query("SELECT * FROM user_info WHERE id = $1", [
+      req.params.id,
+    ]); 
 
     res.render("user-page", {
-        title: user.username,
-        user,
+      title: user.rows[0].username,
+      user: user.rows[0],
     });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = {
-    getMainPage,
-    getUserPageByUid,
+  getMainPage,
+  getUserPageByUid,
 };
